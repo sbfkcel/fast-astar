b254ecb6114d4e655f2f532ae4c1f5cb253c04c3
fan <fan@fandeMacBook-Pro.local>
2019-10-30 10:57:32 +0800

    更新测试用例

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,2 @@
-const Grid = require('./Grid'),
-    Astar = require('./Astar');
-module.exports = {Grid,Astar};
+/*! Project:astar, Create:FWS 2019.10.29 15:21, Update:FWS 2019.10.30 10:56 */ 
+const Grid=require("./Grid"),Astar=require("./Astar");module.exports={Grid:Grid,Astar:Astar};
