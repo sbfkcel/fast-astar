6411642cd62431e900760d6adc9685c2d7cd04a8
fan <fan@fandeMacBook-Pro.local>
2019-10-31 19:31:02 +0800

    _

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,2 @@
-/*! Project:astar, Create:FWS 2019.10.29 15:21, Update:FWS 2019.10.31 19:19 */ 
+/*! Project:astar, Create:FWS 2019.10.29 15:21, Update:FWS 2019.10.31 19:30 */ 
 const Grid=require("./Grid"),Astar=require("./Astar");module.exports={Grid:Grid,Astar:Astar};
