8e91ea061ad95677f23e172792118d21e2ba0d03
fan <fan@fandeiMac.local>
2021-09-12 14:49:40 +0800

    处理查找节点可能导致调用堆栈过深的问题

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,2 @@
-/*! Project:astar, Create:FWS 2019.10.29 15:21, Update:FWS 2019.10.30 10:56 */ 
+/*! Project:astar, Create:FWS 2019.10.29 15:21, Update:FWS 2021.09.12 14:48 */ 
 const Grid=require("./Grid"),Astar=require("./Astar");module.exports={Grid:Grid,Astar:Astar};
